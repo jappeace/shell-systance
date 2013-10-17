20339846bfd7bfbb7913c6ee76cfa5110e1498cc
Jappie Klooster <jappieklooster@hotmail.com>
2013-10-17 22:22:53 +0200

    initial commit

---
+++ execute.sh
@@ -0,0 +1,14 @@
+#if you don't have eix, get it. Its way better then the default emerge search.
+# and it integrates layman rysincing in one command.
+echo "rsyncing" #is alwasy a good idea
+eix-sync
+
+echo "updating evrything and keep on going updating"
+emerge --update --newuse --deep --with-bdeps=y --keep-going=y @world
+
+echo "cleaning dependicies"
+emerge --depclean
+revdep-rebuild
+
+echo "merging config files"
+etc-update
