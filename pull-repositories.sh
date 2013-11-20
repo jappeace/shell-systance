f5129eacbc085883a226d600da5340b9288c279f
Jappie Klooster <jappieklooster@hotmail.com>
2013-11-20 18:36:34 +0100

    better name2 (forgot to add)

---
+++ pull-repositories.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+# put direct git repopository symbolic links in /etc/repository.d/linkname.git
+# or use containing folders like ~/.vim/bundle
+path="/etc/repository.d/"
+for directory in `ls $path`
+do
+	cd $path$directory
+	(
+		sudo git pull
+	)
+	for d in `ls`
+	do
+		(
+			cd $d
+			sudo git pull
+		)
+	done
+done
